39be8551f93a1cd1a9f9754cc3f915bfe3923629
Nik Luparev <nikita.luparev@gmail.com>
2022-06-29 03:56:54 +0500

    if at the start

--- index.ts
+++ index.ts
@@ -92,13 +92,18 @@ function moveVertical(dy: number) {
   }
 }
 
+function handleInput(current: Input) {
+  if (current === Input.LEFT) moveHorizontal(-1);
+  else if (current === Input.RIGHT) moveHorizontal(1);
+  else if (current === Input.UP) moveVertical(-1);
+  else if (current === Input.DOWN) moveVertical(1);
+}
+
 function handleInputs(): void {
   while (inputs.length > 0) {
     let current = inputs.pop();
-    if (current === Input.LEFT) moveHorizontal(-1);
-    else if (current === Input.RIGHT) moveHorizontal(1);
-    else if (current === Input.UP) moveVertical(-1);
-    else if (current === Input.DOWN) moveVertical(1);
+
+    handleInput(current);
   }
 }
 
